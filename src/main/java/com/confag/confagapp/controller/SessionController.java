016dfc9190d18a36218bde1d103c8a806a179e80
patrick ojunde <patrickojunde@gmail.com>
2021-02-27 16:47:31 +0100

    updates

--- src/main/java/com/confag/confagapp/controller/SessionController.java
+++ src/main/java/com/confag/confagapp/controller/SessionController.java
@@ -1,10 +1,39 @@
 package com.confag.confagapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.confag.confagapp.model.Session;
+import com.confag.confagapp.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionController {
+
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Session> getSessions(){
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping("/{session_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Session getASession(@PathVariable long session_id){
+        return sessionRepository.getOne(session_id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+
 
 }
