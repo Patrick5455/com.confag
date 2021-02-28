6d32bc9bf3af621e937653d9820a77cdb9ee4b5f
patrick ojunde <patrickojunde@gmail.com>
2021-02-28 06:36:08 +0100

    update POM to get a standlaone executable jar

--- package-deployment.sh
+++ package-deployment.sh
@@ -2,4 +2,6 @@
 mvn package
 
 # if you are running this project from an ide, close before running this command as it might throw an error of port already in use
-java -jar target/confagapp-0.0.1-SNAPSHOT.jar
+#java -jar target/confagapp-0.0.1-SNAPSHOT.jar #-windows users
+
+sudo ./target/confagapp-0.0.1-SNAPSHOT.jar #- unix users (MacOS, Linux)
